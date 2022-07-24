870b3256b1040b58483a708b5a38d9549e20be84
Saeed Kolivand <saeedkolivand1997@gmail.com>
2022-07-25 01:03:50 +0430

    fix: upgrade dependencies, add ErrorBoundary to App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "app/redux/store";
 import Home from "pages/home/Home";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ErrorBoundary from "app/errorBoundary/ErrorBoundary";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,14 +13,14 @@ const queryClient = new QueryClient({
   },
 });
 
-const App: React.FC = () => {
-  return (
+const App: React.FC = () => (
+  <ErrorBoundary>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <Home />
       </QueryClientProvider>
     </Provider>
-  );
-};
+  </ErrorBoundary>
+);
 
 export default App;
